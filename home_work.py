92cd16bd993fad03210b311cdbad801db7459770
Alya <Aliyak2008@mail.ru>
2023-06-29 16:10:17 +0300

    Функция

--- home_work.py
+++ home_work.py
@@ -1,6 +1,8 @@
-s = input()
-a = s[::-1]
-if s == a:
-  print("True")
-else:
-  print("False")
+def my_funk(s):
+  a = s[::-1]
+  if s == a:
+    return True
+  else:
+    return False
+
+#C:/Users/now3/PycharmProjects/pythonProject17
