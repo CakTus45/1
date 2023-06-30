02a764bd448c3820700f54618c0b570c3a6e59bc
Alya <Aliyak2008@main.ru>
2023-06-30 12:55:32 +0300

    Без ветвления

--- home_work.py
+++ home_work.py
@@ -1,8 +1,4 @@
 def is_palindrom(line):
-  backwards = line[::-1]
-  if line == backwards:
-    return True
-  else:
-    return False
+  return line == line[::-1]
 
 #C:/Users/now3/PycharmProjects/pythonProject17
